b70471b3c87c603ad32fc99c8d6f0c69f59452f3
LFLATH <ludwiglechtreck@gmail.com>
2023-02-11 13:54:31 -0500

    Front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SimpleMap from './Component/SimpleMap';
+function App() {
+  return (
+    <div className="App">
+        <SimpleMap />
+    </div>
+  );
+}
+
+export default App;
+
